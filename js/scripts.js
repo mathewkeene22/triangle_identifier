183eb94bf21bd97581e1862d9bd923b492af8e3c
Mathew Keene <mathewkeene22@gmail.com>
2015-03-20 13:31:48 -0700

    second spec passing for equilateral

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 var triangles = function(one, two, three) {
+  var triangle = "";
 
   if (( one + two < three ) || ( two + three < one ) || ( one + three < two )) {
-    return "That is not a triangle, let alone a polygon."
-  };
+    triangle = "That is not a triangle, let alone a polygon."
+  }
+
+  else if ((one === two) && (two === three) && (three === one)) {
+    triangle =  "Equilateral Triangle"
+  }
+
+  return triangle;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,7 @@ describe('triangles', function() {
     expect(triangles(4, 4, 10)).to.equal("That is not a triangle, let alone a polygon.")
   });
 
+  it("is an equilateral triangle if all three sides are equal", function() {
+    expect(triangles(7, 7, 7)).to.equal("Equilateral Triangle")
+  });
 });
