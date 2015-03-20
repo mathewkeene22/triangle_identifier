26a9ffafcdaabebf5e75b91642531b57d1b93898
Mathew Keene <mathewkeene22@gmail.com>
2015-03-20 14:37:17 -0700

    added jQuery, still working on not a triangle function with jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 var triangles = function(one, two, three) {
   var triangle = "";
 
-  if ((one + two < three) || (two + three < one) || (one + three < two)) {
-    triangle = "That is not a triangle, let alone a polygon."
-  }
-
-  else if ((one === two) && (two === three) && (three === one)) {
-    triangle =  "Equilateral Triangle"
+  if ((one === two) && (two === three) && (three === one)) {
+    triangle = "Equilateral Triangle"
   }
 
   else if ((one !== two) && (two !== three) && (three !== one)) {
@@ -16,6 +12,24 @@ var triangles = function(one, two, three) {
   else if ((one === two) || (two === three) || (one === three)) {
     triangle = "Isosceles Triangle"
   }
-    
+
+  if ((one + two < three) || (two + three < one) || (one + three < one)) {
+    triangle = "That is not a triangle, let alone a polygon."
+  }
+
   return triangle;
 };
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+
+    var one = parseInt($("input#one").val());
+    var two = parseInt($("input#two").val());
+    var three = parseInt($("input#three").val());
+    var result = triangles(one, two, three);
+
+    $("#result").text(result);
+
+    event.preventDefault();
+  });
+});
